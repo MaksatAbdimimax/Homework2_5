ad93830498fa22ac9852b1f1d418fbf9894520b6
unknown <maksatprograms@gmail.com>
2024-01-22 17:50:57 +0600

    Initial commit

---
+++ app/src/main/java/com/geeks/homework2_5/MainActivity.java
@@ -0,0 +1,30 @@
+package com.geeks.homework2_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        EditText editText = findViewById(R.id.edit_text);
+        TextView textview = findViewById(R.id.text_view);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                textview.setText(text);
+
+            }
+        });
+    }
+}
